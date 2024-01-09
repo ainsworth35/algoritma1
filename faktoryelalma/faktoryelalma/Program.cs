f7d99b5326510df10ca888a289bc6015b4528807
ainsworth35 <ahmet_turgut35@outlook.com>
2024-01-09 16:46:29 +0300

    Add files via upload

---
+++ faktoryelalma/faktoryelalma/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace faktoryelalma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen faktoryeli alınacak sayıyı giriniz: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = 1; 
+            for (int i = 1; i <= x; i++)
+            {
+                y *= i;
+            }
+            Console.WriteLine(x + "! = " + y  );
+            Console.ReadKey();
+        }
+    }
+}
